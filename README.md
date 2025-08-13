afc6f7dc6e7d23e4fdb4e457053a56f335f5248a
CJ Cummings <cj@formata.io>
2025-08-13 15:51:52 -0600

    v0.11.0

--- README.md
+++ README.md
@@ -1,27 +1,19 @@
 # Stof CLI
 [Command line interface](https://docs.stof.dev/reference/cli) for [Stof](https://stof.dev).
 
-[Stof](https://stof.dev) is a simple and embeddable data runtime. Expanding on hypermedia concepts, Stof documents make working with data much more enjoyable by unifying many data types and APIs to be used together from one place.
+[Stof](https://stof.dev) is the simplest way to create, store, share, and transform unified data.
 
 - [Docs](https://docs.stof.dev)
 - [Discord](https://discord.gg/Up5kxdeXZt)
-- [Contact Us](https://stof.dev/contact-us)
-- [Introduction & Design](https://docs.stof.dev/book/introduction-and-design)
-- [More Information](https://docs.stof.dev/resources-and-information)
-
-## Why
-> Unified data that manipulates itself is the foundation for portable, composable, secure, and universal APIs.
-
-Combining APIs and manipulating the data to, from, and within them is a pain, made worse by the plethora of programming languages and SDKs that are used.
-
-Stof is a glue format for putting all of this together and a standard interface from your language of choice (or standalone) for working with data and APIs.
-
-If you're sending APIs and data over a network, combining many APIs, or are using a lot of different data types at once, Stof might be a good choice for your project.
+- [Contact](https://stof.dev/contact-us)
 
 ## Example
-> Note: Stof documents are also a superset of JSON
+> Stof also parses normal JSON object syntax
+
+> Check out the examples (and test 'src/model/formats/stof/tests') folder, with real examples for you to play with
+
 ``` rust
-users: [
+const list users: [              // optional type and const specification for fields
     {
         name: "Joe Schmo",       // commas or semi-colons accepted, but optional
         cm height: 6ft + 1in     // Stof adds units and declarations are expressions
@@ -29,17 +21,24 @@ users: [
     },                           // trailing commas in arrays are okay
 ]
 
-fn getJoe(): obj {               // Stof adds data types (casting, etc..)
-    for (user in self.users) {
-        if (user.name.toLower().contains("joe")) return user;
+fn joe() -> obj {                // functions are document data, just like fields
+    for (const user in self.users) {
+        if (user.name.lower().contains("joe")) return user;
     }
-    return null;
+    null
 }
 
-#[main]
+#[main]                          // main attribute to mark this func for 'run'
+#[custom({'ex': true})]          // metadata values (funcs, maps, objs, etc.)
 fn main() {
-    let joe = self.getJoe();
-    pln(stringify(joe, 'toml')); // any format loaded into the doc (parse too)
+    const joe = self.joe();
+    assert(this.attributes().get("custom").get("ex"));
+    
+    async {                                // async at the core (funcs & exprs too)
+        let body = stringify("toml", joe); // any loaded format (binary & parse too)
+        body.push("stof = true\n");
+        pln(body);
+    }
 }
 ```
 ``` bash
@@ -47,26 +46,31 @@ fn main() {
 age = 32
 height = 185.42
 name = "Joe Schmo"
+stof = true
 ```
 
-## Why Use Stof?
-Use Stof for just about any data interchange use-case. It's particularly helpful for data unification, sending APIs (logic) over the wire, and combining APIs dynamically.
+## Motivations
+1. If you've ever wished you could put different data formats into a singular document and manipulate it all at once with a standard (and modern) interface, you're in the right spot.
 
-As an example, it would require many SDKs, dependencies, and a lot of application dependant logic to use the Google Drive API to get document data (images, PDF, DOCX, JSON, etc.), send unified data to an LLM provider, then post a response in Slack or Discord.
+2. If you've wanted a singular API that could be used across servers and within different environments, well now you can.
 
-All of this plus the user configuration, conversation data, and auth can be contained within a singular Stof document, sent between servers, persisted in databases or to disk (even partially), and be executed anywhere Stof runs (most places due to WebAssembly).
+3. If you've been looking for that YAML or JSON replacement/addition that adds a simple programming layer and type system that just makes sense, keep reading.
 
-1. Data Unification
-    - Use different types of data together at once (PDFs, JSON, YAML, XML, Images, DOCX, etc).
-2. Remote Execution
-    - Send Stof over networks as a lightweight and sandboxed way to execute logic remotely.
-3. Data Validation
-    - Use Stofs type system to quickly and easily filter/modify/validate data according to your needs.
-4. Data Transformation & Connectors
-    - Upgrade and downgrade data to and from Stof using the formats of your choice for interoperability, transformations, translations, and data connectors.
+4. If you have an embedded environment where you need to run user sent or untrusted code (especially over the wire), you're pretty good at looking for solutions because you're here.
+
+5. If you've ever thought to yourself "code is just data itself, so why can't I work with it as such?", then you're also in the right place.
+
+## How
+Stof can look and feel like a familiar programming interface, but it's actually just a document of data that comes with a runtime for manipulating itself in a sandbox you control.
+
+Because it's just a normal document of data, it can be shared, stored, combined, split, and otherwise transformed, and seamlessly works with other formats (both binary and text import/export).
+
+We say "data" instead of "field" (like other formats) because a field is just one type of data component (functions are another). Stof is built like an Entity Component System, where components (data) can be anything you'd like, even large PDF documents, 3D models, or binary voice data. All can then be organized cleanly and worked with via Stof functions at the same time.
 
 ## Contributing
-Theres a lot to do, so please jump in and consider supporting the project.
+We have an awesome and growing community, so jump in and consider supporting the project.
+
+Any fellow programming nerds are probably filled with a lot of good ideas at this point, so lets chat (and please get involved)! Email info@stof.dev or find me via GitHub profile.
 
 ## License
 Apache 2.0. See LICENSE for details.
--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,10 @@
 // limitations under the License.
 //
 
-mod publish;
-use publish::{create_pkg_zip, publish_package, unpublish_package};
-
-mod add;
-use add::{add_package, remove_package};
-
-mod remote;
-use remote::{remote_exec, remote_exec_doc, remove_remote_user, set_remote_user};
-
-use std::{fs, path::PathBuf, sync::Arc};
+use std::{collections::HashSet, path::PathBuf};
 use clap::{Parser, Subcommand};
 use colored::Colorize;
-use stof::{gitbook::Gitbook, lang::SError, SDoc};
+use stof::{model::{Graph, StofPackageFormat}, runtime::Error};
 
 
 #[derive(Parser, Debug)]
@@ -40,351 +31,110 @@ struct Cli {
 #[derive(Subcommand, Debug)]
 enum Command {
     /// Run a file or package, calling all #[main] functions.
-    /// Optionally, this command can be used to run stof remotely.
     Run {
-        /// Path to file or package directory to run.
+        /// Path to a file or package to import.
         path: Option<String>,
-
-        /// Run on a remote server.
-        #[arg(short, long, value_name = "ADDRESS")]
-        on: Option<String>,
-
-        /// Full remote command?
-        #[arg(short, long)]
-        full_remote: bool,
-
-        /// When running on a remote server, should the file/package be parsed locally?
-        #[arg(short = 'l', long)]
-        parse_local: bool,
-
-        /// Library allow list. Ex. "http" enables the HTTP library.
-        #[arg(short, long)]
-        allow: Vec<String>,
-
-        /// Optional remote username.
-        #[arg(short, long)]
-        username: Option<String>,
-
-        /// Optional remote password.
-        #[arg(short, long)]
-        password: Option<String>,
     },
 
     /// Test a file or package, running all #[test] functions.
     Test {
-        /// Path to file or package directory to test.
+        /// Path to a file or package to import.
         path: Option<String>,
-
-        /// Library allow list. Ex. "http" enables the HTTP library.
-        #[arg(short, long)]
-        allow: Vec<String>,
     },
 
-    /// Create documentation for a file or package.
+    /// Create documentation for a file or package using the "docs" format.
     Docs {
-        /// Path to file or package directory to test.
+        /// Path to a directory or file to import.
         path: Option<String>,
 
-        /// Optional output path.
+        /// Optional document output directory.
         out: Option<String>,
-
-        /// HTML option.
-        #[arg(long)]
-        html: bool,
-    },
-
-    /// Remove a package from this workspace.
-    Remove {
-        /// Package to remove.
-        package: String,
     },
 
     /// Create a package file (.pkg) from a directory that contains a pkg.stof file.
     Pkg {
-        /// Package directory (containing pkg.stof file) to turn into a package file (.pkg).
-        dir: String,
+        /// Path to a directory (with a pkg.stof file).
+        path: Option<String>,
 
         /// Optional output file path (.pkg).
-        /// Default is <DIR>.pkg.
+        /// Default is <PATH>/out.pkg.
         out: Option<String>,
     },
-
-    /// Publish this package to each registry defined in the pkg.stof publish array.
-    Publish {
-        /// Package directory, containing pkg.stof file.
-        /// Default is to use the current working directory.
-        dir: Option<String>,
-
-        /// Registry name.
-        /// Registry with a #[default] attribute is used by default.
-        #[arg(short, long)]
-        registry: Option<String>,
-
-        /// Optional remote username.
-        #[arg(short, long)]
-        username: Option<String>,
-
-        /// Optional remote password.
-        #[arg(short, long)]
-        password: Option<String>,
-    },
-
-    /// Unpublish this package from each registry defined in the pkg.stof publish array.
-    Unpublish {
-        /// Package directory, containing pkg.stof file.
-        /// Default is to use the current working directory.
-        dir: Option<String>,
-
-        /// Registry name.
-        /// Registry with a #[default] attribute is used by default.
-        #[arg(short, long)]
-        registry: Option<String>,
-
-        /// Optional remote username.
-        #[arg(short, long)]
-        username: Option<String>,
-
-        /// Optional remote password.
-        #[arg(short, long)]
-        password: Option<String>,
-    },
-
-    /// Add a remote package to this workspace, placed within the __stof__ directory for import access via "@path" syntax.
-    Add {
-        /// Package to add.
-        package: String,
-
-        /// Package directory, containing pkg.stof file.
-        /// Default is to use the current working directory.
-        dir: Option<String>,
-
-        /// Registry name.
-        /// Registry with a #[default] attribute is used by default.
-        #[arg(short, long)]
-        registry: Option<String>,
-
-        /// Optional remote username.
-        #[arg(short, long)]
-        username: Option<String>,
-
-        /// Optional remote password.
-        #[arg(short, long)]
-        password: Option<String>,
-    },
-
-    /// Create or update a user on a specific runner.
-    SetRemoteUser {
-        /// Remote server address.
-        server: String,
-
-        /// Admin username.
-        admin_user: String,
-
-        /// Admin password.
-        admin_pass: String,
-
-        /// New user username.
-        username: String,
-
-        /// New user password.
-        password: String,
-
-        /// Permissions (0b1001 -> exec, delete, write, read).
-        /// Default is 9, exec + read.
-        #[arg(short, long)]
-        perms: Option<i64>,
-
-        /// Scope for this user, restricting the modification (write or delete) paths this user has access to.
-        /// For example, a scope of "example" would allow this user (if permitted) to write and delte only the registry paths that start with "@example/".
-        #[arg(short, long)]
-        scope: Option<String>,
-    },
-
-    /// Remove a user on a specific runner.
-    DeleteRemoteUser {
-        /// Remote server address.
-        server: String,
-
-        /// Admin username.
-        admin_user: String,
-
-        /// Admin password.
-        admin_pass: String,
-
-        /// Username to remove.
-        username: String,
-    }
 }
 
-
-#[tokio::main]
-async fn main() {
+/// Main.
+fn main() {
     let cli = Cli::parse();
     match cli.command {
-        Command::Run { path, on, full_remote, allow, parse_local, username, password } => {
-            // Execute the entire run command remotely if requested
-            if !parse_local && on.is_some() {
-                if let Some(remote_address) = on {
-                    if let Some(path) = path {
-                        remote_exec(&remote_address, full_remote, &path, username, password).await;
-                    } else {
-                        remote_exec(&remote_address, full_remote, "", username, password).await;
-                    }
-                    return;
-                }
-            }
-
-            // Create the document that will be ran
-            let doc;
+        Command::Run { path } => {
+            let mut graph;
             if let Some(path) = path {
-                doc = create_doc(&path, &allow);
+                graph = create_graph(&path);
             } else {
-                doc = create_doc("", &allow);
+                graph = create_graph("");
             }
-
-            // Execute this document remotely
-            if let Some(remote_address) = on {
-                remote_exec_doc(&remote_address, full_remote, &doc, username, password).await;
-                return;
-            }
-
-            // Run the document locally
-            let res = SDoc::run_blocking_async(doc, None, None).await;
-            match res {
-                Ok(_) => {
-                    // Nothing to do here...
-                },
-                Err(res) => println!("{res}"),
-            }
-        },
-        Command::Test { path, allow } => {
-            let doc;
-            if let Some(path) = path {
-                doc = create_doc(&path, &allow);
-            } else {
-                doc = create_doc("", &allow);
-            }
-
-            let res = SDoc::test_blocking_async(doc, false, None).await;
-            match res {
+            match graph.run(None, true) {
                 Ok(res) => println!("{res}"),
                 Err(res) => println!("{res}"),
             }
         },
-        Command::Docs { path, out, html } => {
-            let doc;
-            let mut out_path;
-            let allow = vec!["docs".to_string()];
+        Command::Test { path } => {
+            let mut graph;
             if let Some(path) = path {
-                doc = create_doc(&path, &allow);
-
-                let mut path = path.split('.').collect::<Vec<&str>>();
-                if path.len() > 1 {
-                    path.pop();
-                }
-
-                if html {
-                    out_path = format!("{}.html", path.join("."))
-                } else {
-                    out_path = format!("{}.md", path.join("."))
-                }
+                graph = create_graph(&path);
             } else {
-                doc = create_doc("", &allow);
-                if html {
-                    out_path = "docs.html".to_string();
-                } else {
-                    out_path = "docs.md".to_string();
-                }
+                graph = create_graph("");
             }
-
+            match graph.test(None, true) {
+                Ok(res) => println!("{res}"),
+                Err(res) => println!("{res}"),
+            }
+        },
+        Command::Docs { path, out } => {
+            let mut out_path = String::from("./");
             if let Some(out) = out {
                 out_path = out;
             }
 
-            let md = doc.export_string("main", "gitbook", None);
-            match md {
-                Ok(mut out) => {
-                    if html {
-                        out = markdown::to_html(&out);
-                    }
+            let mut in_path = String::default();
+            if let Some(path) = path {
+                in_path = path;
+            }
 
-                    let mut path = out_path.split('/').collect::<Vec<&str>>();
-                    path.pop();
-                    if path.len() > 0 {
-                        let dirpath = path.join("/");
-                        let _ = fs::create_dir_all(&dirpath);
-                    }
-
-                    if let Err(err) = fs::write(out_path, out) {
-                        println!("{err:?}");
-                    } else {
-                        println!("{}", "ok".green());
-                    }
+            let graph = create_graph(&in_path);
+            match graph.docs(&out_path, None) {
+                Ok(_) => {
+                    println!("{} {}", "created docs".green(), out_path.blue());
                 },
-                Err(err) => {
-                    println!("{}", err.to_string(&doc.graph));
+                Err(error) => {
+                    println!("{} {}", "docs creation error".red(), error.to_string());
                 }
             }
         },
-        Command::Publish { dir, registry, username, password } => {
-            let mut pkg_dir = "./".to_string();
-            if let Some(dir) = dir {
-                pkg_dir = dir;
+        Command::Pkg { path, out } => {
+            let mut dir = "".to_string();
+            if let Some(path) = path {
+                dir = path;
             }
-            publish_package(&pkg_dir, registry, username, password).await;
-        },
-        Command::Unpublish { dir, registry, username, password } => {
-            let mut pkg_dir = "./".to_string();
-            if let Some(dir) = dir {
-                pkg_dir = dir;
-            }
-            unpublish_package(&pkg_dir, registry, username, password).await;
-        },
-        Command::Add { dir, registry, package, username, password } => {
-            let mut pkg_dir = "./".to_string();
-            if let Some(dir) = dir {
-                pkg_dir = dir;
-            }
-            add_package(&pkg_dir, &package, registry, false, username, password).await;
-        },
-        Command::Remove { package } => {
-            if remove_package(&package).await {
-                println!("{} {}", "removed".green(), package.blue());
-            } else {
-                println!("{} {}", "failed to remove".red(), package.blue());
-            }
-        },
-        Command::Pkg { dir, out } => {
-            let mut out_path = dir.clone();
+
+            let mut out_path = format!("{dir}/out.pkg");
             if let Some(out) = out {
                 out_path = out;
             }
-            if let Some(path) = create_pkg_zip(&dir, &out_path, true).await {
+            let included = HashSet::new();
+            let excluded = HashSet::new();
+            if let Some(path) = StofPackageFormat::create_package_file(&dir, &out_path, &included, &excluded) {
                 println!("{} {}", "created".green(), path.blue());
             } else {
                 println!("{}", "pkg creation error".red());
             }
         },
-        Command::SetRemoteUser { server, admin_user, admin_pass, username, password, perms, scope } => {
-            let mut user_perms: i64 = 0b1001; // read and exec only access by default
-            if let Some(prm) = perms {
-                user_perms = prm;
-            }
-            let mut user_scope = String::default();
-            if let Some(sc) = scope {
-                user_scope = sc;
-            }
-            set_remote_user(&server, &admin_user, &admin_pass, &username, &password, user_perms, &user_scope).await;
-        },
-        Command::DeleteRemoteUser { server, admin_user, admin_pass, username } => {
-            remove_remote_user(&server, &admin_user, &admin_pass, &username).await;
-        }
     }
 }
 
 
-/// Create a stof document from a file path.
-fn create_doc(path: &str, allow: &Vec<String>) -> SDoc {
+/// Create a stof graph from a file path.
+fn create_graph(path: &str) -> Graph {
     let path_buf;
     if path.len() > 0 {
         path_buf = PathBuf::from(path);
@@ -394,57 +144,28 @@ fn create_doc(path: &str, allow: &Vec<String>) -> SDoc {
         panic!("{} {}: {}", "parse error".red(), path.blue(), "no directory or path found".dimmed());
     }
     
-    let mut doc = SDoc::default();
-    allow_libs(&mut doc, allow);
+    let mut graph = Graph::default();
 
     let res;
     if path_buf.is_dir() {
-        res = doc.file_import("main", "pkg", path_buf.to_str().unwrap(), "stof", "");
+        res = graph.file_import("pkg", path_buf.to_str().unwrap(), None);
     } else if let Some(format) = path_buf.extension() {
         if let Some(format) = format.to_str() {
-            res = doc.file_import("main", format, path_buf.to_str().unwrap(), format, "");
+            res = graph.file_import(format, path_buf.to_str().unwrap(), None);
         } else {
-            res = Err(SError::custom("main", &doc, "FormatError", "could not retrieve import format"));
+            res = Err(Error::Custom("could not retrieve import format".into()));
         }
     } else {
-        res = Err(SError::custom("main", &doc, "FormatError", "could not determin import extension"));
+        res = Err(Error::Custom("could not determin import extension".into()));
     }
 
     match res {
         Ok(_) => {
-            doc
+            graph
         },
         Err(error) => {
-            eprintln!("{} {}: {}", "parse error".red(), path.blue(), error.message.dimmed());
-            SDoc::default()
+            eprintln!("{}", error.to_string());
+            Graph::default()
         }
-    }
-}
-
-
-/// Allow libraries.
-/// Because this is the CLI, the File System is enabled by default.
-/// stof --allow all FILE_PATH
-fn allow_libs(doc: &mut SDoc, allow: &Vec<String>) {
-    let mut http_enabled = false;
-    for name in allow {
-        match name.as_str() {
-            "all" => {
-                http_enabled = true;
-            },
-            "http" => {
-                http_enabled = true;
-            },
-            "docs" => {
-                doc.load_stof_docs();
-                doc.load_format(Arc::new(Gitbook{}));
-            },
-            _ => {
-                println!("{}: {}", "unrecognized library".italic().dimmed(), name.purple());
-            }
-        }
-    }
-    if !http_enabled {
-        doc.remove_library("Http");
     }
 }
